64337a80da7a10e81c81e99f7f99809207e40722
Artem Nurtdinov <artem.nefrit@gmail.com>
2026-01-04 22:35:34 +0300

    SqlAlchemyFollowAgeUnitOfWorkFactory, SqlAlchemyChatMessageUnitOfWorkFactory refactoring

--- core/types.py
+++ core/types.py
@@ -1,8 +1,8 @@
 from contextlib import AbstractContextManager
-from typing import Protocol
+from typing import Any, Protocol
 
 from sqlalchemy.orm import Session
 
 
 class SessionFactory(Protocol):
-    def __call__(self) -> AbstractContextManager[Session]: ...
+    def __call__(self, *args: Any, **kwargs: Any) -> AbstractContextManager[Session]: ...
