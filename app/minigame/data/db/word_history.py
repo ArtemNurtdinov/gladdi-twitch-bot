e32f547d3b5f261d9be2c51abf5e7ad0344dfb21
Artem Nurtdinov <artem.nefrit@gmail.com>
2026-01-12 19:13:39 +0300

    refactoring word_history.py, word_history_repository.py

--- app/minigame/data/db/word_history.py
+++ app/minigame/data/db/word_history.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 
-from sqlalchemy import Column, DateTime, Integer, String
+from sqlalchemy import DateTime, Integer, String
+from sqlalchemy.orm import Mapped, mapped_column
 
 from core.db import Base
 
@@ -8,7 +9,7 @@ from core.db import Base
 class WordHistory(Base):
     __tablename__ = "word_history"
 
-    id = Column(Integer, primary_key=True, autoincrement=True)
-    channel_name = Column(String, nullable=False, index=True)
-    word = Column(String, nullable=False, index=True)
-    created_at = Column(DateTime, default=datetime.utcnow, nullable=False)
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True, nullable=False)
+    channel_name: Mapped[str] = mapped_column(String, nullable=False, index=True)
+    word: Mapped[str] = mapped_column(String, nullable=False, index=True)
+    created_at: Mapped[datetime] = mapped_column(DateTime, default=datetime.utcnow, nullable=False)
