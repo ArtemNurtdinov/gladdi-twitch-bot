c19d7781ec0963ae6e7d5a6f814389647d572c0d
Artem Nurtdinov <artem.nefrit@gmail.com>
2026-01-14 19:44:23 +0300

    remove unused import

--- app/twitch/presentation/twitch_routes.py
+++ app/twitch/presentation/twitch_routes.py
@@ -10,7 +10,7 @@ from app.platform.bot.bot_manager import BotManager
 from app.platform.bot.bot_settings import DEFAULT_SETTINGS, BotSettings, build_bot_settings
 from app.platform.bot.schemas import BotActionResult, BotStatus
 from app.twitch.bootstrap.router_factory import build_twitch_command_router
-from app.twitch.bootstrap.twitch import build_twitch_api_service, build_twitch_providers
+from app.twitch.bootstrap.twitch import build_twitch_providers
 from app.twitch.infrastructure.chat.twitch_chat_client import TwitchChatClient
 from app.twitch.infrastructure.helix.auth import TwitchAuth
 from app.twitch.presentation.twitch_schemas import AuthStartResponse
