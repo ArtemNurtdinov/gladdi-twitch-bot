eb7012d3cea64ce88fac25cee8dc4715f1cca85d
Artem Nurtdinov <artem.nefrit@gmail.com>
2026-01-16 02:07:38 +0300

    refactoring HandleTokenCheckerUseCase

--- app/platform/application/handle_token_checker_use_case.py
+++ app/platform/application/handle_token_checker_use_case.py
@@ -14,11 +14,9 @@ class HandleTokenCheckerUseCase:
     def interval_seconds(self) -> int:
         return self._interval_seconds
 
-    async def handle(self) -> str | None:
+    async def handle(self) -> None:
         token_is_valid = await self._platform_auth.check_token_is_valid()
         logger.info(f"Статус токена: {'действителен' if token_is_valid else 'недействителен'}")
         if not token_is_valid:
             await self._platform_auth.update_access_token()
             logger.info("Токен обновлён")
-            return "token_refreshed"
-        return None
