d568c5160a4a1df4b193a8ec678b0a0bb99845f6
Artem Nurtdinov <artem.nefrit@gmail.com>
2026-01-20 17:17:32 +0300

    add word_history_uow.py

--- app/minigame/application/add_used_word_use_case.py
+++ app/minigame/application/add_used_word_use_case.py
@@ -1,12 +1,13 @@
-from app.minigame.domain.repo import WordHistoryRepository
+from app.minigame.application.word_history_uow import WordHistoryUnitOfWorkFactory
 
 
 class AddUsedWordsUseCase:
-    def __init__(self, repo: WordHistoryRepository):
-        self._repo = repo
+    def __init__(self, unit_of_work_factory: WordHistoryUnitOfWorkFactory):
+        self._unit_of_work_factory = unit_of_work_factory
 
     def add_used_words(self, channel_name: str, word: str):
         normalized = "".join(ch for ch in str(word).lower() if ch.isalpha())
         if not normalized:
             return
-        return self._repo.add_word(channel_name, normalized)
+        with self._unit_of_work_factory.create() as uow:
+            return uow.word_history_repo.add_word(channel_name, normalized)
--- app/minigame/application/get_used_words_use_case.py
+++ app/minigame/application/get_used_words_use_case.py
@@ -1,10 +1,10 @@
-from app.minigame.domain.repo import WordHistoryRepository
+from app.minigame.application.word_history_uow import WordHistoryUnitOfWorkFactory
 
 
 class GetUsedWordsUseCase:
-    def __init__(self, repo: WordHistoryRepository):
-        self._repo = repo
+    def __init__(self, unit_of_work_factory: WordHistoryUnitOfWorkFactory):
+        self._unit_of_work_factory = unit_of_work_factory
 
     def get_used_words(self, channel_name: str, limit: int) -> list[str]:
-        words = self._repo.list_recent_words(channel_name, limit)
-        return words
+        with self._unit_of_work_factory.create(read_only=True) as uow:
+            return uow.word_history_repo.list_recent_words(channel_name, limit)
