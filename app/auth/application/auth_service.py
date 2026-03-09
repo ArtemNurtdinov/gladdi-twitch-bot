fbf684e90f72fc50b0f9428670fda73fda690e96
Artem Nurtdinov <artem.nefrit@gmail.com>
2026-03-09 23:32:03 +0300

    refactoring

--- app/auth/application/auth_service.py
+++ app/auth/application/auth_service.py
@@ -2,7 +2,6 @@ from uuid import UUID
 
 from app.auth.application.dto import (
     TokenDto,
-    UserCreateDto,
     UserDto,
     UserUpdateDto,
 )
@@ -10,7 +9,7 @@ from app.auth.application.mapper.token_mapper import TokenMapper
 from app.auth.application.mapper.user_mapper import UserMapper
 from app.auth.application.ports.password_hasher import PasswordHasher
 from app.auth.domain.auth_repository import AuthRepository
-from app.auth.domain.models import UserCreateData, UserUpdateData
+from app.auth.domain.models import UserUpdateData
 
 
 class AuthService:
@@ -26,16 +25,6 @@ class AuthService:
         self._user_mapper = user_mapper
         self._token_mapper = token_mapper
 
-    def _to_domain_create(self, user_data: UserCreateDto) -> UserCreateData:
-        return UserCreateData(
-            email=user_data.email,
-            first_name=user_data.first_name,
-            last_name=user_data.last_name,
-            password=user_data.password,
-            role=user_data.role,
-            is_active=user_data.is_active,
-        )
-
     def _to_domain_update(self, user_data: UserUpdateDto) -> UserUpdateData:
         return UserUpdateData(
             email=user_data.email,
@@ -45,13 +34,6 @@ class AuthService:
             role=user_data.role,
             is_active=user_data.is_active,
         )
-
-    def create_user_from_admin(self, user_data: UserCreateDto) -> UserDto:
-        domain_input = self._to_domain_create(user_data)
-        hashed_password = self._password_hasher.hash_password(user_data.password)
-
-        user = self._repo.create_user(domain_input, hashed_password)
-        return self._user_mapper.map_user_to_dto(user)
 
     def update_user(self, user_id: UUID, user_data: UserUpdateDto) -> UserDto | None:
         updates = self._to_domain_update(user_data)
