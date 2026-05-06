3e8bd8ef3b3469cccf2c0446131e4459d60a7d7e
Artem Nurtdinov <artem.nefrit@gmail.com>
2026-05-06 11:59:53 +0300

    refactoring

--- app/core/network/api/client.py
+++ app/core/network/api/client.py
@@ -9,14 +9,14 @@ from app.core.network.api.model.response import ApiResponse
 class ApiClient(ABC):
     _TIMEOUT_SECONDS_DEFAULT = 10.0
     _MAX_CONNECTIONS_DEFAULT = 20
-    _MAX_KEEP_ALIVIE_CONNECTIONS_DEFAULT = 10
+    _MAX_KEEP_ALIVE_CONNECTIONS_DEFAULT = 10
 
     def __init__(self, base_url: str):
         self._client = httpx.AsyncClient(
             base_url=base_url,
             timeout=httpx.Timeout(self._TIMEOUT_SECONDS_DEFAULT),
             limits=httpx.Limits(
-                max_connections=self._MAX_CONNECTIONS_DEFAULT, max_keepalive_connections=self._MAX_KEEP_ALIVIE_CONNECTIONS_DEFAULT
+                max_connections=self._MAX_CONNECTIONS_DEFAULT, max_keepalive_connections=self._MAX_KEEP_ALIVE_CONNECTIONS_DEFAULT
             ),
         )
 
